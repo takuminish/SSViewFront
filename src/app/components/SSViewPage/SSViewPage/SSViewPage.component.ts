b4d3c739c0442cd23e4184705f2f2a7863d30820
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-08-16 13:17:20 +0900

    SS個別表示画面の作成

--- src/app/components/SSViewPage/SSViewPage/SSViewPage.component.ts
+++ src/app/components/SSViewPage/SSViewPage/SSViewPage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SS } from 'src/app/models/SS.model';
 
 @Component({
   selector: 'app-SSViewPage',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./SSViewPage.component.scss']
 })
 export class SSViewPageComponent implements OnInit {
+
+  public ss:SS = {
+    id: 1,
+    title: 'SSタイトル',
+    content: 'テスト本文です。テスト本文です。テスト本文です。テスト本文です。テスト本文です。テスト本文です。',
+    tags: ['タグ1', 'タグ2', 'タグ3', 'タグ4', 'タグ5', 'タグ6', 'タグ7', 'タグ8'],
+    pvcount: 1,
+    createdAt: new Date(),
+  }
 
   constructor() { }
 
